742dd2afffc08ecec14485cc6c5aac4bcd3749e8
wenchao <liwenchaotz@gmail.com>
2019-12-23 14:36:50 +0800

    fix exports

--- dist/es2015/index.d.ts
+++ dist/es2015/index.d.ts
@@ -1,5 +1,5 @@
 export { Constructor, Class, RuntimeClassDecorator, IntValuedEnum, StringValuedEnum } from './esextends';
 export { Generator, ParameterizedGenerator, Supplier, Producer } from './generator';
-export { UnaryOperator, Transformer, AsyncTransformer, Predicate, Consumer } from './operator/unary';
+export { UnaryOperator, Transformer, AsyncTransformer, Predicate, AsyncPredicate, Consumer } from './operator/unary';
 export { BinaryOperator, BiTransformer, AsyncBiTransformer, Joiner, AsyncJoiner, BiConsumer, Comparator, Reducer, Accumulator } from './operator/binary';
 export { DataSource, Being } from './data';
